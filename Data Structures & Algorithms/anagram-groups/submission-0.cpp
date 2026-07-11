4640fc122f3f7370e9793f75bb5fe3dd07131f23
Yushuo Feng <66173381+FullToker@users.noreply.github.com>
2026-07-12 00:48:33 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        for(auto &str:strs){
+            string key = str;
+            sort(key.begin(),key.end());
+            groups[key].push_back(str);
+        }
+        vector<vector<string>> res;
+        for(auto &item:groups){
+            res.push_back(item.second);
+        }
+        return res;
+    }
+};
